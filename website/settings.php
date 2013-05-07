ba9cac67513df15f920af9096b99478d4bcaf565
StephanieWalter <stephiewalter89@googlemail.com>
2013-05-07 18:09:10 +0200

    update settings page

---
+++ website/settings.php
@@ -0,0 +1,15 @@
+<?
+include('header.php');
+?>
+
+<div class="container">
+	<div class="span5">
+          <h2>Settings</h2>
+          <p>User name:</p>
+		  <p>E-Mail:</p>
+    </div>
+</div>	
+
+<?
+include('footer.php');
+?>
