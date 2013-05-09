8df43f840157ae12bba7c2da39ee6ac3872044c5
Dennis Wilhelm <dwilhelm89@googlemail.com>
2013-05-09 22:28:14 +0200

    Contributors site and content for index

--- website/index.php
+++ website/index.php
@@ -27,11 +27,11 @@ include('header-start.php');
 
   <div class="container leftband">
     <div class="row-fluid">
-      <div class="span6">
-        <h1>car.io</h1>
-        <p>Here goes the eyecatching marketing slogan!.<br> Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui..</p>
+      <div class="span5 offset1">
+        <p><img style="width:40%" src="../assets/img/play.svg"/></p>
+        <h2>Watch our video </h2>
       </div>
-      <div class="span6">
+      <div class="span5">
             <h2 class="form-signin-heading">Please sign in</h2>
             <input type="text" id="login_name" class="input-block-level" placeholder="User name">
             <input type="password" id="login_password" class="input-block-level" placeholder="Password">
@@ -45,15 +45,21 @@ include('header-start.php');
 	<div class="container rightband">
 	<div class="row-fluid">
         <div class="span4">
-          <h2>Keep track over your Costs</h2>
-          <p>car.io helps you to automatically calculate your gas costs.</p>
+          <h2>Get our App!</h2>
+          <a href="https://play.google.com/store/apps/details?id=enviroCar">
+            <img alt="Get it on Google Play" style="margin-left: 50px; margin-top: 10px;"
+                 src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" />
+          </a>
        </div>
-        <div class="span4">
-          <h2>Watch our Video!</h2>
-        </div>
         <div class="span4">
           <h2>Be a Citizen Scientist!</h2>
           <p>Help the world become a better place by sharing your data with scientists from all over the world! Or use existing data for your own research!</p>
+        </div>
+        <div class="span4">
+          <h2>Support us on Indiegogo</h2>
+          <a href="">
+            <img style="width:70%;" src="http://www.pcgameshardware.de/screenshots/811x455/2012/12/igg_logo_color_print_black_h.jpg"/>
+          </a>
         </div>
       </div>
 
@@ -64,7 +70,7 @@ include('header-start.php');
 
 
       <div class="featurette">
-        <img class="featurette-image pull-right" src="../assets/img/examples/browser-icon-chrome.png">
+        <img class="featurette-image pull-right" src="../assets/img/heatmap.PNG" style="margin-top:-80px;">
         <h2 class="featurette-heading">enviroCar <span class="muted">Make our cities smarter!</span></h2>
         <p class="lead">This is a community, it's an app and it's a website.<br> enviroCar is our contribution to a smarter world.<br> We will generate knowledge about car traffic and its emissions on our streets and we will raise awareness of environmental consequences of our driving behaviour.</p>
       </div>
@@ -72,7 +78,7 @@ include('header-start.php');
       <hr class="featurette-divider">
 
       <div class="featurette">
-        <img class="featurette-image pull-left" src="../assets/img/examples/browser-icon-firefox.png">
+        <img class="featurette-image pull-left" style="height:50%" src="../assets/img/paper_architecture.png">
         <h2 class="featurette-heading">How does it work? <span class="muted">Three steps to become a citizen scientist</span></h2>
         <p class="lead">
           <ul>
