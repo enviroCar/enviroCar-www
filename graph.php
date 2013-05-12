52b82bb3ff1133195778892fac7c494d751d7630
Dennis Wilhelm <dwilhelm89@googlemail.com>
2013-05-12 14:46:40 +0200

    new graph page

---
+++ graph.php
@@ -0,0 +1,36 @@
+<?
+include('header.php');
+?>
+
+
+<!-- https://google-developers.appspot.com/chart/interactive/docs/gallery/linechart -->
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance'
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+<div class="container">
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+</div>
+
+
+<?
+include('footer.php');
+?>
+
