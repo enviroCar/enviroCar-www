b4a9071464c3bfb1fa02b8219d18cce331b7c39b
oraibalmegdadi <almegdadi.oraib@gmail.com>
2013-05-16 14:01:53 +0200

    Create main_index.php

---
+++ main_index.php
@@ -0,0 +1,83 @@
+<?
+include('header_new.php');
+?>
+
+
+<!--o-->
+<div class="container rightband">
+<h2 style="text-align: center"> Make Our Cities Smarter ... HOW ??!!??</h2>
+</div>
+
+<!-- The Carousel-->
+<div class="container">
+    <div class="row-fluid">
+<div class="span10 offset1" style="border: 3px groove">
+  <div id="myCarousel" class="carousel slide" data-interval="3000" >
+
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+  <!-- Carousel items -->
+  <div class="carousel-inner">
+    <div class="active item" ><img src="./assets/img/image1.jpg" ></div>
+    <div class="item"  ><img src="./assets/img/image2.jpg"></div>
+    <div class="item"  ><img src="./assets/img/image3.jpg"></div>
+  </div>
+  <!-- Carousel nav -->
+  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+</div></div>
+</div></div>
+
+<!--End of  The Carousel-->
+<div class="container leftband">
+<h2 style="text-align: center"> Be a Citizen Scientist ... HOW ??!!??</h2>
+</div>
+
+
+<!--video and graph statistics -->
+<div class="container">
+
+<div class="span5 ">
+  <iframe src="http://www.youtube.com/embed/FXD3eVooo2U" style="width:90%; height:58%" frameborder="7"  allowfullscreen></iframe>
+ </div>
+ <!--end of video -->
+<div class="span5 offset1">
+         <img src="./assets/img/graph_stat.png" style="width:90%;" ></iframe>
+  	 
+		   </div>
+ <!--end of Graph statistics -->
+ </div>
+<!--END of video and graph statistics -->
+
+<div class="container rightband">
+</br>
+<div class="row-fluid">
+
+        <div class="span4">
+          <h2>Get our App!</h2>
+          <a href="https://play.google.com/store/apps/details?id=enviroCar">
+            <img alt="Get it on Google Play" style="margin-left: 50px; margin-top: 10px;"
+                 src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" />
+          </a>
+       </div>
+        <div class="span4">
+         
+         <img style="height:35%" src="./assets/img/paper_architecture.png">
+		 <h2>Drive & Share !</h2>
+        </div>
+        <div class="span4">
+          <h2>Support us on Indiegogo</h2>
+          <a href="">
+            <img style="width:70%;" src="http://www.pcgameshardware.de/screenshots/811x455/2012/12/igg_logo_color_print_black_h.jpg"/>
+          </a>
+        </div>
+      </div>
+</div>
+
+
+<?
+include('footer.php');
+?>
