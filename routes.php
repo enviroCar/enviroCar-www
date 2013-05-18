768745bbc3a58f4ef33a937c8faada8633ebf972
Dennis Wilhelm <dwilhelm89@googlemail.com>
2013-05-18 14:46:56 +0200

    Changes based on new JSON String
    + Deleting unused files

--- routes.php
+++ routes.php
@@ -3,8 +3,8 @@ include('header.php');
 ?>
 
 <script type="text/javascript">
-	function addRoutes(id, titel){
-      $('#routes').append('<li class="customLi"><img src="./assets/img/route.svg" style="height: 30px; margin-right: 10px; "/><a href="route.php?id='+id+'">'+id+'</a></li>');
+	function addRoutes(id, name, date){
+      $('#routes').append('<li class="customLi"><img src="./assets/img/route.svg" style="height: 30px; margin-right: 10px; "/><a href="route.php?id='+id+'">'+name+'</a><br><div>Created: '+date+'</div></li>');
     }
 
     $.get('./assets/includes/users.php?tracks', function(data) {
@@ -13,10 +13,27 @@ include('header.php');
       	}else{
         	data = JSON.parse(data);
     		for(i = 0; i < data.tracks.length; i++){
-    			addRoutes(data.tracks[i].id, "");
+    			addRoutes(data.tracks[i].id, data.tracks[i].name, convertToLocalTime(data.tracks[i].modified));
     		}
     	}
   	});
+
+  	function convertToLocalTime(serverDate) {
+	    var dt = new Date(Date.parse(serverDate));
+	    var localDate = dt;
+
+	    var gmt = localDate;
+	        var min = gmt.getTime() / 1000 / 60; // convert gmt date to minutes
+	        var localNow = new Date().getTimezoneOffset(); // get the timezone
+	        // offset in minutes
+	        var localTime = min - localNow; // get the local time
+
+	    var dateStr = new Date(localTime * 1000 * 60);
+		var d = dateStr.getDate();
+	    var m = dateStr.getMonth() + 1;
+	    var y = dateStr.getFullYear();
+	    return '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+	}
 
 </script>
 
