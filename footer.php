dfd7fde86a28201f21f30d0b42fc0eab8ec4b8ea
Dustin Demuth <mail@dustindemuth.de>
2013-05-22 17:46:27 +0200

    added links to imprint in the footer.php

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
-      <!-- FOOTER -->
-	  
+<!-- FOOTER -->  
 	<footer>
 		<div class="container leftband">
         <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2013 enviroCar &middot; <a href="#">Terms</a></p>
+        <span class="featurette-heading">2013 enviroCar</span>
+        <span> &middot; <a href="imprint.php">Imprint</a></span>
+        <span> &middot; <a href="#">Terms of Use</a><span>
+        <span> &middot; <a href="#">Privacy Policy</a><span>
 		</div>
 	</footer>
 	
-
-    <!-- Le javascript
-    ================================================== -->
+	<!-- Javascripts -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="./assets/js/bootstrap-transition.js"></script>
     <script src="./assets/js/bootstrap-alert.js"></script>
@@ -21,7 +21,6 @@
     <script src="./assets/js/bootstrap-popover.js"></script>
     <script src="./assets/js/bootstrap-button.js"></script>
     <script src="./assets/js/bootstrap-collapse.js"></script>
-    <script src="./assets/js/bootstrap-carousel.js"></script>
     <script src="./assets/js/bootstrap-typeahead.js"></script>
 
   </body>
