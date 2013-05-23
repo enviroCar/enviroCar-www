f7a869e42c6d6edd7a82c367cd51e682ec287e74
FriedrichMueller <mue-friedrich@web.de>
2013-05-23 15:36:48 +0200

    index.php with language functionality

---
+++ lang_en.php
@@ -0,0 +1,33 @@
+<?php 
+// index.php
+$index_cont1="Invalid Login Name"; 
+$index_cont2="Password cannot be empty"; 
+$index_cont3="Access denied!";
+$index_cont4="Please sign in";
+$index_cont5="Password";
+$index_cont6="User details";
+$index_cont7="Register";
+$index_cont8="Get our App soon!";
+$index_cont9="Get it on Google Play!";
+$index_cont10="Be a Citizen Scientist";
+$index_cont11="Help the world become a better place by sharing your data with scientists from all over the world! Or use existing data for your own research!";
+$index_cont12="Support us on Indiegogo";
+$index_cont13="This is a community, it's an app and it's a website.<br> enviroCar is our contribution to a smarter world.<br> We will generate knowledge about car traffic and its emissions on our streets and we will raise awareness of environmental consequences of our driving behaviour.";
+$index_cont14="How does it work?";
+$index_cont15="Three steps to become a citizen scientist";
+$index_cont16="Register";
+$index_cont17="Sign in";
+$index_cont18="User name";
+
+
+
+
+
+
+
+
+
+
+
+
+?>
