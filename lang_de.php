09fbe69bcb61b2d5ed542f2cbc796bec7da76eb9
FriedrichMueller <mue-friedrich@web.de>
2013-05-23 15:36:48 +0200

    index.php with language functionality

---
+++ lang_de.php
@@ -0,0 +1,32 @@
+<?php 
+// index.php
+$index_cont1="Ungültiger Benutzername"; 
+$index_cont2="Bitte tragen sie ein Passwort ein"; 
+$index_cont3="Zugriff verweigert!";
+$index_cont4="Bitte tragen Sie sich ein!";
+$index_cont5="Passwort";
+$index_cont6="Benutzerangaben";
+$index_cont7="Register";
+$index_cont8="Holen Sie sich unsere App!";
+$index_cont9="Besuchen Sie Google Play!";
+$index_cont10="Tragen Sie ihren Teil zur Wissenschaft bei!";
+$index_cont11="Machen Sie die Welt zu einem besseren Ort, indem Sie ihre erfassten Daten mit Wissenschaftler auf der ganzen Welt teilen! Oder forschen Sie mit bereits vorhandenen Daten!";
+$index_cont12="Unterstützen Sie uns auf Indiegogo";
+$index_cont13="Dies ist eine Community, eine App für ihr Handy und es ist eine Webseite. EnviroCar ist unser Beitrag für eine intelligentere Welt. Wir werden Wissen über den Automobilverkehr und der damit verbundenen Abgasemission auf unseren Straßen generieren. Und ein Bewusstsein schaffen welche Auswirkungen unser Fahrverhalten auf die Umwelt besitzt.";
+$index_cont14="Wie funktioniert das Ganze?";
+$index_cont15="Drei Schritte und Sie sind dabei.";
+$index_cont16="Registrieren";
+$index_cont17="Anmelden";
+$index_cont18="Benutzername";
+
+
+
+
+
+
+
+
+
+
+
+?>
