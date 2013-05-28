f1b2b64454580a6060d8f1b3dce11bcb77ea245b
oraibalmegdadi <almegdadi.oraib@gmail.com>
2013-05-28 11:19:56 +0300

    Update support_new.php

--- support_new.php
+++ support_new.php
@@ -15,8 +15,8 @@ $('#myTab a:last').tab('show');
 
 
 </script>
-<div class="container">
-<div class="row">
+<div class="container-fluid">
+<div class="row-fluid">
 <div class="tabbable">
 
     <ul id="myTab" class="nav nav-tabs" >
@@ -25,7 +25,7 @@ $('#myTab a:last').tab('show');
       <li><a href="#website" data-toggle="tab"> <strong> WebSite</strong></a></li>
       <li><a href="#faqs" data-toggle="tab"><strong> FAQs</strong></a></li>
     </ul>
-  </div>
+	</div>
     <div class="tab-content">
 	
       <div class="tab-pane fade in active" id="main">
@@ -55,7 +55,7 @@ $('#myTab a:last').tab('show');
 				Join the community and help to make our world a little smarter, traffic less stressful, and economic driving more fun!
 				</p>
 				
-				<p class="pull-right"><a href="#">Back to top</a></p>
+				<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
 
          </div>    
            </div>    
@@ -70,11 +70,11 @@ $('#myTab a:last').tab('show');
 				 various sensors of your car. The enviroCar App installed on your smartphone is able to connect automatically
 				 to that OBD2 adapter to read out parameters from your car, such as current fuel consumption or revolutions 
 				 per minute. This way, enviroCar can accurately measure your driving costs, can compute emitted CO2 or even 
-				 estimate the noise created by your car.
-				</br> <img class="offset2" src="./assets/img/obd_adapter.png" height="200" width="400" >
+				 estimate the noise created by your car.</br>
+				</br> <img class="offset2" src="./assets/img/obd_adapter.png" height="200" width="400" ></br>
 				</br>By using OBD2 adapter, enviroCar is able to receive your cars data by pairing your smartphone via Bluetooth with the adapter. The smartphone app analyzes and parses the data to give you in the app and on the enviroCar these information, which you want to see.
 				</p>
-				<p class="pull-right"><a href="#">Back to top</a></p>
+				<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
              </div>    
            </div>    
          </div>    
@@ -97,7 +97,7 @@ $('#myTab a:last').tab('show');
 							registration Steps ... </br> .. </br> ... </br> .. </br>... </br> .. </br>
 					</p>
 					
-					<p class="pull-right"><a href="#">Back to top</a></p>
+					<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
 			 </div>    
 			   </div>    
             </div>    
@@ -110,7 +110,7 @@ $('#myTab a:last').tab('show');
 							Downloading Steps ... </br> .. </br> ... </br> .. </br>... </br> .. </br>
 					</p>
 					
-					<p class="pull-right"><a href="#">Back to top</a></p>
+					<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
 			 </div>    
 			   </div>    
             </div>    
@@ -123,7 +123,7 @@ $('#myTab a:last').tab('show');
 							synchronization Steps ... </br> .. </br> ... </br> .. </br>... </br> .. </br>
 					</p>
 					
-					<p class="pull-right"><a href="#">Back to top</a></p>
+					<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
 			 </div>    
 			   </div>    
             </div>    
@@ -137,8 +137,180 @@ $('#myTab a:last').tab('show');
       <!--end of div id="website"-->
 	  <div class="tab-pane fade in " id="faqs">
 	  
+	  <div class="row-fluid">
+
+				<ul>
+					<li><a href="#q1"> Q1: How does it work?</a></li>		
+					<li><a href="#q2"> Q2: How to get started? What are the requirements?</a></li>
+					<li><a href="#q3"> Q3: Which OBD-II adapters are compatible with enviroCar?</a></li>		
+					<li><a href="#q4"> Q4: Is my car supported?</a></li>
+					<li><a href="#q5"> Q5: Why do we need Indiegogo backers?</a></li>		
+					<li><a href="#q6"> Q6: What’s about privacy and security?</a></li>
+					<li><a href="#q7"> Q7: I’m a developer and very interested in this project. Is there a way to make some further implementations?</a></li>
+					<li><a href="#q8"> Q8: What’s next?</a></li>		
+					<li><a href="#q9"> Q9: When enviroCar will be published?</a></li>	
+					<li><a href="#q10"> Q10: Can we ship to your country?</a></li>	
+
+
+					
+				</ul>
+				</div>
+ <div class="container rightband">
+ <div class="row-fluid">
+		<div class="span10 offset1">
+	  		<div class="customLi"><h5 name="q1">How does it work?</h5>
+			
+			<p style="text-align: justify; font-size:14px;">  
+				By using OBD2 adapter, enviroCar is able to receive your cars data by pairing your smartphone via Bluetooth with the adapter. The smartphone app analyzes and parses the data to give you in the app and on the enviroCar these information, which you want to see.
+			</p>
+			<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
+         </div>    
+           </div>    
+         </div>    
+         </div>
+		 
+<div class="container leftband">
+ <div class="row-fluid">
+		<div class="span10 offset1">
+	  		<div class="customLi"><h5 id="q2">How to get started? What are the requirements?</h5>
+				 <p style="text-align: justify; font-size:14px;"> Plug your OBD2 adapter into the OnBoard Diagnostic (OBD) port of your car. 
+				 Download & install the app, pair it with your adapter and you’re ready to be a part of enviroCar.
+</p>
+				<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
+             </div>    
+           </div>    
+         </div>    
+         </div>
+		 
+	  
+	  
 	  </div>
-<!--end of div id="faqs"-->   
+
+	  
+ <div class="container rightband">
+ <div class="row-fluid">
+		<div class="span10 offset1">
+	  		<div class="customLi"><h5 name="q3">Which OBD-II adapters are compatible with enviroCar?</h5>
+			
+			<p style="text-align: justify; font-size:14px">  
+				There are quite a lot OBD-II adapters for using enviroCar in different pricy ranges. You can buy these adapters on ebay, amazon or several shops. To get some infomation, you can get q quick overview. But please keep in mind, there are many (for example on ebay or amazon) which won’t work. Please have a look on some reviews. If you want to be sure, 
+Also one very important fact about OBD-II use adapters in Germany: It’s only allowed to use CE-certified OBD-II adpaters. While driving your car, the usage of adapters without a CE-certificate is permitted. If you want to be sure, to have the right adapter, contact us or make a pledge for the 199€ pledge (enviroCar device (early bird)), where the right adapter is already included.
+
+				</p>
+				
+				<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
+
+         </div>    
+           </div>    
+         </div>    
+         </div>
+		 
+<div class="container leftband">
+ <div class="row-fluid">
+		<div class="span10 offset1">
+	  		<div class="customLi"><h5 id="q4">Is my car supported?</h5>
+				 <p style="text-align: justify; font-size:14px"> Mainly there is the following rule. In the USA, all cars from 01.01.1996 have to have the OBD2 interface.
+In Europe, all new cars with an Otto-motor (using benzine) from 01.01.2001 and with the EURO-3 Norm have the OBD2 interface. For diesel vehicles from 01.01.2003 OBD-2 is supported. For trucks, the OBD2 support started with 1. January 2005
+You can also have a look here
+Alternatively we could use a picture like this: http://www.obd-2.de/faqs/obd-2/51-ist-mein-fahrzeug-obd-2-kompatibel.html 
+</p>
+				<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
+             </div>    
+           </div>    
+         </div>    
+         </div>
+		 
+	  
+	  
+	  </div>
+
+	 <div class="container rightband">
+ <div class="row-fluid">
+		<div class="span10 offset1">
+	  		<div class="customLi"><h5 name="q5">Why do we need Indiegogo backers? </h5>
+			
+			<p style="text-align: justify; font-size:14px">  
+			By using indiegogo we want to collect 10.000€ for our future work. With your donation,
+			you can be sure, that the enviroCar has a future. That you have the chance to review your and other 
+			data for a long time; that you can make yourself and your city more smarter. Help us, that people and 
+			students can extend and work on this project. 
+			(Linked Citizen science is a topic, in which the ifgi and 52N want to do more scientific work) 
+				</p>
+				
+				<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
+
+         </div>    
+           </div>    
+         </div>    
+         </div>
+		 
+<div class="container leftband">
+ <div class="row-fluid">
+		<div class="span10 offset1">
+	  		<div class="customLi"><h5 id="q6">What’s about privacy and security?</h5>
+				 <p style="text-align: justify; font-size:14px">Of course we are aware of your privacy issue. All your data will be anonymized, 
+				 so nobody will be able to collect sensitive, specific, person-related data about your usage. Your security is a very important 
+				 topic for us. By using enviroCar you haven’t worry about it. </p>
+				 <p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
+             </div>    
+           </div>    
+         </div>    
+         </div>
+		 
+	  
+	  
+	  </div>
+
+	 <div class="container rightband">
+ <div class="row-fluid">
+		<div class="span10 offset1">
+	  		<div class="customLi"><h5 name="q7"> I’m a developer and very interested in this project. Is there a way to make some further implementations?</h5>
+			
+			<p style="text-align: justify; font-size:14px"> Open Source software, sparql, contact us</p>
+				
+				<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
+
+         </div>    
+           </div>    
+         </div>    
+         </div>
+		 <div class="container leftband">
+ <div class="row-fluid">
+		<div class="span10 offset1">
+	  		<div class="customLi"><h5 id="q8">What’s next?</h5>
+				 <p style="text-align: justify; font-size:14px">To start with our goal to make your place a little bit smarter, you’re the most important person in this program. The next steps include further implementation of new features and the development a working system as well as building up the enviroCar community.</p>
+				 <p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
+             </div>    
+           </div>    
+         </div>    
+         </div>
+		 
+	  
+	  
+	  </div>
+
+	 <div class="container rightband">
+ <div class="row-fluid">
+		<div class="span10 offset1">
+	  		<div class="customLi"><h5 name="q9"> When enviroCar will be published?</h5>
+			
+			<p style="text-align: justify; font-size:14px"> We’re working hard on the enviroCar system. For now we’ve implemented first prototypes for app and website. Further work will be on adding more features and implementing useful functions for you.
+We plan to release enviroCar with the end of XXX. Please be patient and look for our updates.</p>
+				
+				<p class="pull-right" style="font-size:10px"><a href="#">Back to top</a></p>
+
+         </div>    
+           </div>    
+         </div>    
+         </div>
+
+		 
+	  
+	  
+	  </div>
+
+		
+	<!--end of div id="faqs"-->   
    </div>
    
 	    </div>
