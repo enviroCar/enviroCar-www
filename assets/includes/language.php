a3add848ecb89c1b725175ccd5a69d3f8c1e8544
Dustin Demuth <mail@dustindemuth.de>
2013-05-30 19:23:21 +0200

    added support for language header

--- assets/includes/language.php
+++ assets/includes/language.php
@@ -1,6 +1,14 @@
 <?
 if (!isset($_SESSION)) session_start();
 
+//First: Get language from Browser:
+
+	require_once('./httplanguage/httplanguage.php');
+	//get language from accept-language header
+	$allowed_langs = array ('de', 'en');
+	$lang = lang_getfrombrowser ($allowed_langs, 'en', null, false);
+
+//Second: Look if BrowserChoice has been overridden By User input
 if(isSet($_GET['lang'])){
 	$lang = $_GET['lang'];
 
@@ -15,10 +23,6 @@ else if(isSet($_SESSION['lang'])){
 else if(isSet($_COOKIE['lang'])){
 	$lang = $_COOKIE['lang'];
 }
-else{
-	$lang = 'en';
-}
-
 
 
 include('lang_'.$lang.'.php');
