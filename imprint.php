caabbd02be71cbe0fbc92ee8f9e4ee38900daea6
Dustin Demuth <mail@dustindemuth.de>
2013-05-31 14:46:12 +0200

    formatted

--- imprint.php
+++ imprint.php
@@ -6,20 +6,22 @@ include('header-start.php');
 		<div class="row-fluid">
 			<div class="span">
 				<h2 id="ImprintHead"><? echo $imprint_imprint;?></h2>
-				52&deg; North Initiative for Geospatial Open Source Software GmbH<br/>
-				Martin-Luther-King-Weg 24<br/>
-				48155 Münster, Germany<br/>
-				<br/>
-				<? echo $imprint_phone;?> +49 (0)251.396371-0<br/>
-				<? echo $imprint_fax;?> +49 (0)251.396371-11<br/>
-				<? echo $imprint_email;?> info@52north.org<br/>
-				http://52north.org<br/>
-				<br/>
-				<? echo $imprint_directors;?> Dr. Albert Remke, Dr. Andreas Wytzisk<br/>
-				<br/>
-				USt.-ID-Nr. DE253303206<br/>
-				<br/>
-				<? echo $imprint_registry;?> Amtsgericht Münster HRB 10849<br/>
+				<p>
+					52&deg; North Initiative for Geospatial Open Source Software GmbH<br/>
+					Martin-Luther-King-Weg 24<br/>
+					48155 Münster, Germany<br/>
+					<br/>
+					<? echo $imprint_phone;?> +49 (0)251.396371-0<br/>
+					<? echo $imprint_fax;?> +49 (0)251.396371-11<br/>
+					<? echo $imprint_email;?><a href="mailto:info@52north.org">info@52north.org</a><br/>
+					<a href="http://52north.org">http://52north.org</a><br/>
+					<br/>
+					<? echo $imprint_directors;?> Dr. Albert Remke, Dr. Andreas Wytzisk<br/>
+					<br/>
+					USt.-ID-Nr. DE253303206<br/>
+					<br/>
+					<? echo $imprint_registry;?> Amtsgericht Münster HRB 10849<br/>
+				</p>
 			</div>
 		</div>
 	</div>
@@ -28,7 +30,9 @@ include('header-start.php');
 		<div class="row-fluid">
 			<div class="span">
 				<h2 id="ContentHead"><? echo $imprint_content;?></h2>
-				<? echo $imprint_contenttext;?>
+				<p style="text-align: justify">
+					<? echo $imprint_contenttext;?>
+				</p>
 			</div>
 		</div>
 	</div>
@@ -37,7 +41,9 @@ include('header-start.php');
 		<div class="row-fluid">
 			<div class="span">
 				<h2 id="CopyrightHead"><? echo $imprint_copyright;?></h2>
-				<? echo $imprint_copyrighttext;?>
+				<p style="text-align: justify">
+					<? echo $imprint_copyrighttext;?>
+				</p>
 			</div>
 		</div>
 	</div>
