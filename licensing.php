1b7247c7d4ea4a901de2fb0b4010c7ab1a1cf814
Dustin Demuth <mail@dustindemuth.de>
2013-06-02 11:56:47 +0200

    margins

--- licensing.php
+++ licensing.php
@@ -4,27 +4,33 @@ include('header-start.php');
 
 	<div class="container leftband">
 		<div class="row-fluid">
-			<div class="span" style="margin-right: 1%">
+			<div class="span">
 				<h2 id="ImprintHead"><?echo $licensing_datalicensing; ?></h2>
+				<p style="text-align: justify; margin-right: 1%">
 				<?echo $licensing_datalicensingtext; ?>
+				</p>
 			</div>
 		</div>
 	</div>
 
 	<div class="container leftband">
 		<div class="row-fluid">
-			<div class="span" style="margin-right: 1%">
+			<div class="span">
 				<h2 id="ImprintHead"><?echo $licensing_allowed; ?></h2>
+				<p style="text-align: justify; margin-right: 1%">
 				<?echo $licensing_allowedtext; ?>
+				</p>
 			</div>
 		</div>
 	</div>
 	
 	<div class="container leftband">
 		<div class="row-fluid">
-			<div class="span" style="margin-right: 1%">
+			<div class="span">
 				<h2 id="ImprintHead"><?echo $licensing_aslongas; ?></h2>
+				<p style="text-align: justify; margin-right: 1%">
 				<?echo $licensing_aslongastext; ?>
+				</p>
 			</div>
 		</div>
 	</div>
