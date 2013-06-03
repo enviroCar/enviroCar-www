968548d68ac9ec0d3bfb8a44521595b021c09843
dwilhelm89 <dwilhelm89@googlemail.com>
2013-06-03 08:48:26 +0200

    Update header.php

--- header.php
+++ header.php
@@ -95,7 +95,7 @@ function echoActiveClassIfRequestMatches($requestUri)
               <li <?=echoActiveClassIfRequestMatches("friends")?>><a href="friends.php"><? echo $friends ?></a></li>
               <li <?=echoActiveClassIfRequestMatches("groups")?>><a href="groups.php"><? echo $groups ?></a></li>
               <li <?=echoActiveClassIfRequestMatches("community")?>><a href="community.php"><? echo $community ?></a></li>
-              <li <?=echoActiveClassIfRequestMatches("help")?>><a href="support_new.php"><? echo $help ?></a></li>
+              <li <?=echoActiveClassIfRequestMatches("support_new")?>><a href="support_new.php"><? echo $help ?></a></li>
             </ul>
           </div><!--/.nav-collapse -->
         </div>
