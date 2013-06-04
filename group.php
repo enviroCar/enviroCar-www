39fbb26bff40028776aa89151a974bef0fcf9f86
Dennis Wilhelm <dwilhelm89@googlemail.com>
2013-06-04 19:59:49 +0200

    Group Activities

--- group.php
+++ group.php
@@ -32,6 +32,10 @@ include('header.php');
   			})
   		}
 
+  		function addGroupActivities(actionImg, friendImg, id, titel){
+      		$('#groupActivities').append('<li class="customLi"><img src="'+actionImg+'" style="height: 30px; margin-right: 10px; "/><a href="'+id+'">'+titel+'</a><img src="'+friendImg+'" style="height: 30px; margin-right: 10px; float:right; "/></li>');
+    	}
+
   		$.get('./assets/includes/groups.php?groupMembers=<? echo $_GET['group'] ?>', function(data) {
 	      	if(data >= 400){
 	          error_msg("Group members couldn't be loaded successfully.");
@@ -56,6 +60,31 @@ include('header.php');
 	      	}
 	  	});
 
+	  	$.get('./assets/includes/groups.php?groupActivities=<? echo $_GET['group'] ?>', function(data) {
+	      	if(data >= 400){
+	          error_msg("Group activities couldn't be loaded successfully.");
+	      	}else{
+		        data = JSON.parse(data);
+	          	if(data.activities.length > 0){
+		            for(i = 0; i < data.activities.length; i++){
+		              	var activity = data.activities[i];
+		              	console.log(activity);
+		              	if(activity.type == "JOINED_GROUP"){
+		                	addGroupActivities("./assets/img/person.svg","./assets/img/user.jpg", "group.php?group="+activity.group.name, "Joined: "+activity.group.name);
+		              	}else if(activity.type == "CREATED_GROUP"){
+		                	addGroupActivities("./assets/img/person.svg","./assets/img/user.jpg", "group.php?group="+activity.group.name, "Created: "+activity.group.name);
+		              	}else if(activity.type == "FRIENDED_USER"){
+		                	addGroupActivities("./assets/img/user.jpg","./assets/img/user.jpg", "profile.php?user="+activity.other.name, "Friended: "+activity.other.name);
+		              	}else if(activity.type == "CREATED_TRACK"){
+		                	addGroupActivities("./assets/img/route.svg","./assets/img/user.jpg", "track.php?id="+activity.track.properties.id, "Created: "+activity.track.properties.name);
+		              	}
+		            }
+		        }else{
+		          $('#groupActivities').append("No recent activities available");
+		        }   
+	      	}
+	  	});
+
 
   	</script>
 	
@@ -65,39 +94,17 @@ include('header.php');
 			</div>
 
 
-			<div class="span5" style="max-height:400px">
+			<div class="span5">
 				<h2>Members</h2>
-				<ul id="memberList">	
+				<ul id="memberList" style="max-height: 400px; overflow-y: auto;">	
 
 				</ul>          
 	        </div>
 			
 			<div class="span4">
-				<h2><? echo $activities ?></h2>
-				<!--
-			  	<ul>
-			  	<li class="customLi">
-					<a href="">Albert Remke</a>
-					<br/>
-					awards economic driver today
-				</li>
-				<li class="customLi">
-					<a href="">Jakob </a>
-					<br/>
-					moving towards muenster city
-				</li>
-				<li class="customLi">
-					<a href="">Dennis</a>
-					<br/>
-					 spend 20 euro for travelling 100 km
-				</li>
-				<li class="customLi">
-					<a href="">Stephnie</a>
-					<br/>
-					release4 gml co2 during last drive
-				</li>
-			  </ul>
-			-->
+				<h2><? echo $activities ?></h2>	
+			  	<ul id="groupActivities" style="max-height: 400px; overflow-y: auto;">
+			  	</ul>
 	        </div>
 	      </div>
 		</div>
