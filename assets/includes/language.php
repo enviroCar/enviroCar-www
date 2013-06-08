92eef69a0a3d81c8cf053b39d6d10939ad0e34cc
Dustin Demuth <mail@dustindemuth.de>
2013-06-08 14:29:10 +0200

    more fun with cookies

--- assets/includes/language.php
+++ assets/includes/language.php
@@ -15,13 +15,9 @@ if(isSet($_GET['lang'])){
 	// register the session and set the cookie
 	$_SESSION['lang'] = $lang;
 
-	setcookie('lang', $lang, time() + (3600 * 24 * 30));
 }
 else if(isSet($_SESSION['lang'])){
 	$lang = $_SESSION['lang'];
-}
-else if(isSet($_COOKIE['lang'])){
-	$lang = $_COOKIE['lang'];
 }
 
 
