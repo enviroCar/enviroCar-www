f5eea4be9a8f3fc1c3ac2247916a8a6538b70e17
Dennis Wilhelm <dwilhelm89@googlemail.com>
2013-06-12 10:36:32 +0200

    Concat & Uglify Bootstrap dependencies

--- footer.php
+++ footer.php
@@ -12,17 +12,6 @@
 	
 	<!-- Javascripts -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./assets/js/bootstrap-transition.js"></script>
-    <script src="./assets/js/bootstrap-alert.js"></script>
-    <script src="./assets/js/bootstrap-modal.js"></script>
-    <script src="./assets/js/bootstrap-dropdown.js"></script>
-    <script src="./assets/js/bootstrap-scrollspy.js"></script>
-    <script src="./assets/js/bootstrap-tab.js"></script>
-    <script src="./assets/js/bootstrap-tooltip.js"></script>
-    <script src="./assets/js/bootstrap-popover.js"></script>
-    <script src="./assets/js/bootstrap-button.js"></script>
-    <script src="./assets/js/bootstrap-collapse.js"></script>
-    <script src="./assets/js/bootstrap-typeahead.js"></script>
-
+    <script type="text/javascript" src="./assets/js/bootstrap-plugins-concat.min.js"></script>
   </body>
 </html>
