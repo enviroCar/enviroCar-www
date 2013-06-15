bf860e88ac09c98f9bdeafaac0574765c4a5cae8
Dustin Demuth <mail@dustindemuth.de>
2013-06-15 10:49:26 +0200

    preventing empty and not existing languages

--- assets/includes/language.php
+++ assets/includes/language.php
@@ -7,11 +7,11 @@
 	$allowed_langs = array ('de', 'en');
 	$lang = lang_getfrombrowser ($allowed_langs, 'en', null, false);
 
-//Second: Look if BrowserChoice has been overridden By User input
+//Second: Look if BrowserChoice has been overridden By User input, but validate the input!
 if(isSet($_GET['lang'])){
 	if ($_GET['lang'] != "" && in_array(strtolower($_GET['lang']), $allowed_langs)){
 	
-		$lang = $_GET['lang'];
+		$lang = $_GET['lang'];//if input is valid
 
 		if (!isset($_SESSION)) session_start();
 		// register the session and set the cookie
