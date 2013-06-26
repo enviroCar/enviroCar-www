5b7eedaace2bd18a443ac771a803bfaa04879e60
Dennis Wilhelm <dwilhelm89@googlemail.com>
2013-06-26 17:38:57 +0200

    information product page

---
+++ informationproduct.php
@@ -0,0 +1,30 @@
+<?
+include('header.php');
+?>
+
+<div class="container rightband">
+	<div>
+		<h2>Interactive map</h2>
+		Get the <a href="community_lines.php">interactive map</a> showing the aggregated measurements of the enviroCar routes and see where ou contributed to the information products! <br>
+		<i>(It may can take a while until your uploaded route has been integrated into the map.)</i>
+	</div>
+	<br>
+	<div>
+		<h2>Data Analyses</h2>
+		<div class="span4">
+			<p><b>CO2 Emission map</b></p>
+			insert picture here!
+		</div>	
+		<div class="span4">
+			<p><b>Speed map</b></p>
+			insert picture here!
+		</div>
+	</div>
+
+
+</div>
+
+
+<?
+include('footer.php');
+?>
