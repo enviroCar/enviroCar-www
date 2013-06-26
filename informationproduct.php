0a093ae4f250baf01f54934a19c92be3c17af327
Dennis Wilhelm <dwilhelm89@googlemail.com>
2013-06-26 17:44:29 +0200

    information product - minor changes

--- informationproduct.php
+++ informationproduct.php
@@ -2,7 +2,7 @@
 include('header.php');
 ?>
 
-<div class="container rightband">
+<div class="container rightband" style="padding-left:20px">
 	<div>
 		<h2>Interactive map</h2>
 		Get the <a href="community_lines.php">interactive map</a> showing the aggregated measurements of the enviroCar routes and see where ou contributed to the information products! <br>
@@ -11,15 +11,21 @@ include('header.php');
 	<br>
 	<div>
 		<h2>Data Analyses</h2>
-		<div class="span4">
-			<p><b>CO2 Emission map</b></p>
-			insert picture here!
-		</div>	
-		<div class="span4">
-			<p><b>Speed map</b></p>
-			insert picture here!
+		<div class="row-fluid">
+			<div class="span5">
+				<p><b>CO2 Emission map</b></p>
+				insert picture here!
+			</div>	
+			<div class="span4">
+				<p><b>Speed map</b></p>
+				insert picture here!
+			</div>
 		</div>
 	</div>
+	<br>
+	<p>
+		The analyses have been created with common GIS programs. To learn more about how to get access to the enviroCar data, visit the <a href="">developer documentation</a> and create your own products!
+	<p>
 
 
 </div>
