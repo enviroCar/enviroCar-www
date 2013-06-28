2089e6682fd37b9203aa209ba3ea4af14a0ead89
FriedrichMueller <mue-friedrich@web.de>
2013-06-28 23:53:46 +0200

    Extend informationproduct and new asset folder for provided downloads

--- informationproduct.php
+++ informationproduct.php
@@ -12,24 +12,82 @@ include('header.php');
 	</div>
 	<br>
 	<div>
-		<h2>Data Analyses</h2>
+		<h2>Data Analysis</h2>
 		<div class="row-fluid">
 			<div class="span5">
-				<p><b>CO2 Emission map</b></p>
-				insert picture here!
+				<p><b>Overview measurement points</b></p>
+				<img style= "width:100%; height:250px;" src="./assets/img/marketing/measurepoints_overview.jpg" />
 			</div>	
-			<div class="span4">
-				<p><b>Speed map</b></p>
-				insert picture here!
+			<div class="span5">
+				<p><b>Detail of a measurement point</b></p>
+				<img style="width:100%; height: 250px;"  src="./assets/img/marketing/measurepoints_detail.jpg" />
 			</div>
 		</div>
 	</div>
 	<br>
 	<p>
-		The analyses have been created with common GIS programs. To learn more about how to get access to the enviroCar data, visit the <a href="">developer documentation</a> and create your own products!
+		The collected data through the mobile application allow further analysis for example with a GIS program.
+		To learn more about how to get access to the enviroCar data, visit the <a href="">developer documentation</a> and create your own products!<br>
+		For making your own analysis here you can download an example data set as shape file format.<br>
+		<a class="btn" target="_blank" href="assets/download/testdata.shp">Download (Shape)</a>
 	<p>
 
 
+</div>
+
+
+	<br>
+<div class="container rightband" style="padding-left:20px">
+	<div>
+		<h2>Data Visualization</h2>
+		<div class="row-fluid">
+			<div class="span" style="padding-right: 1ex">
+				<p><b>CO2 emission map</b></p>
+				<br>
+				<img class="offset2" style= "width:50%; height:200px;" src="./assets/img/marketing/co2_1.jpg"/>
+			</div>
+		</div>
+            <br>
+        <div class="row-fluid">
+			<div class="span" style="padding-right: 1ex">
+				<p><b>Speed map</b></p>
+				<br>
+				<img class="offset2" style= "width:50%; height:200px;" src="./assets/img/marketing/speed1.jpg"/>
+			</div>
+		</div>
+	<br></br>
+	<p>
+	 The measurement of coordinates and attribute values from the car that is collected through the mobile application like CO2 emission or speed values make further visualizations possible.
+	 In this example the speed or CO2 values along the measurement route are visualized with an interpolation tool that just creates a continuous surface from sampled point values. 
+	<br>
+	<p>
+	     <div class="row-fluid">
+			<div class="span" style="padding-right: 1ex">
+				<p><b>Comparison of Speed and CO2 visualization</b></p>
+				<br>
+				<img class="offset2" style= "width:50%; height:200px;" src="./assets/img/marketing/co2_speed_compare.jpg"/>
+			</div>
+	<br>		
+	<br>
+	<p>
+		Visualization of the measurement data is a useful tool for getting an overview or for making comparisons. Here for example you can easily see that high CO2 values are in the same area of the route as high speed values. 
+		
+	<p>
+	<br>
+		     <div class="row-fluid">
+			<div class="span" style="padding-right: 1ex">
+				<p><b>Speed map detail</b></p>
+				<br>
+				<img class="offset2" style= "width:50%; height:200px;" src="./assets/img/marketing/speed_detail.jpg"/>
+			</div>
+	<br></br>
+	<p>
+		Moreover, the analysis of the measurement can visualize details like speed-up and slow-down areas.
+	<p>
+	
+	
+
+    </div>
 </div>
 
 
