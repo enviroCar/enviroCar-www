da7362b85fb92ff45dacfce1bce6a3d675036426
Dustin Demuth <mail@dustindemuth.de>
2013-07-03 17:14:40 +0200

    min-height for customli, creategroup button text

--- groups.php
+++ groups.php
@@ -103,9 +103,11 @@ if(isset($_GET['group_deleted'])){
 		  </div>
 		  <div class="modal-body">
 		  	<form id="createGroupForm" action="./assets/includes/groups.php?createGroup" method="post">
+				<label for="group_name"><? echo $groupname; ?></label><br/>
 		    	<input id="group_name" type="text" class="input-block-level" placeholder="<? echo $groupname; ?>">
+		    	<label for="group_description"><? echo $groupdescription; ?></label><br/>
 		    	<input id="group_description" type="text" class="input-block-level" placeholder="<? echo $groupdescription; ?>">
-		    	<input type="submit" class="btn btn-primary">
+		    	<input type="submit" class="btn btn-primary" value="<? echo $creategroup;?>">
 		    </form>
 		  </div>
 		  <div class="modal-footer">
