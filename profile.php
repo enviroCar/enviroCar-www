262531dba604992a8ca9e1d67502ed69ce30d61e
Dennis Wilhelm <dwilhelm89@googlemail.com>
2013-07-05 14:41:24 +0200

    Exchanged profile pages

--- profile.php
+++ profile.php
@@ -35,7 +35,7 @@ $user = (isset($_GET['user'])) ? $_GET['user'] : $loggedInUser;
         if(data == 400){
           error_msg("<? echo $personError ?>");
         }else if(data == 401 || data == 403){
-          error_msg("<? echo $personNotAllowed ?>")
+          noFriend();
         }else if(data == 404){
           error_msg("<? echo $personNotFound ?>")
         }
@@ -84,6 +84,13 @@ $user = (isset($_GET['user'])) ? $_GET['user'] : $loggedInUser;
     });
   }
 
+  function noFriend(){
+    $('#comparison').hide();
+    $('#friendsgroups').hide();
+    $('#nofriends').show();
+
+  }
+
 
   function getUserGroups(){
     $.get('./assets/includes/users.php?groupsOf='+user, function(data) {
@@ -91,7 +98,7 @@ $user = (isset($_GET['user'])) ? $_GET['user'] : $loggedInUser;
         if(data == 400){
           error_msg("<? echo $groupError ?>");
         }else if(data == 401 || data == 403){
-          $('#groups').append("<? echo $groupOfNotAllowed ?> "+user+" <? echo $noFriendsYet ?>");
+          noFriend();
         }else if(data == 404){
           error_msg("<? echo $groupNotFound ?>")
         }
@@ -99,11 +106,9 @@ $user = (isset($_GET['user'])) ? $_GET['user'] : $loggedInUser;
       }else{
         data = JSON.parse(data);
         if(data.groups.length > 0 ){
-          $('#groups').append('<ul style="margin-bottom: 10px; overflow-y:auto; max-height: 400px;">');
           for(i = 0; i < data.groups.length; i++){
-            $('#groups').append('<li class="customLi"><a href="group.php?group='+data.groups[i].name+'">'+data.groups[i].name+'</a></li>');
+            $('#groups').append('<li class="customLi" style="height:30px"><a href="group.php?group='+data.groups[i].name+'">'+data.groups[i].name+'</a></li>');
           }
-          $('#groups').append('</ul>');
         }
       }
       $('#loadingIndicator_groups').hide();
@@ -116,7 +121,7 @@ $user = (isset($_GET['user'])) ? $_GET['user'] : $loggedInUser;
         if(data == 400){
           error_msg("<? echo $friendError ?>");
         }else if(data == 401 || data == 403){
-          error_msg("<? echo $friendNotAllowed ?>")
+          noFriend();
         }else if(data == 404){
           error_msg("<? echo $friendNotFound ?>")
         }
@@ -152,7 +157,7 @@ $user = (isset($_GET['user'])) ? $_GET['user'] : $loggedInUser;
         if(data == 400){
           error_msg("<? echo $friendError ?>");
         }else if(data == 401 || data == 403){
-          $('#friends').append("<? echo $friendNotAllowed ?> "+user+" <? echo $noFriendsYet ?>");
+          noFriend();
         }else if(data == 404){
           error_msg("<? echo $friendNotFound ?>")
         }
@@ -295,16 +300,14 @@ $user = (isset($_GET['user'])) ? $_GET['user'] : $loggedInUser;
 
 <div class="container rightband">
   <div class="row-fluid">
-    <div class="span4">
+    <div class="span3">
       <div class="well sidebar-nav">
 		<span style="text-align: center; display: block">
+      <h2><p id="username"></p></h2>
 			<img src="./assets/includes/get.php?url=https://giv-car.uni-muenster.de/stable/rest/users/<? echo $user; ?>/avatar?size=200&amp;auth=true" style="height: 200px; width:200px; margin-right: auto; margin-left: auto;" alt="<? echo $user;?>"/>
 		</span>
-        <ul class="nav nav-list">
-          <li><b id="username"></b></li>
-        </ul>
-        
-        <ul id="userInformation" class="nav nav-list"></ul>
+    <br>
+        <ul id="userInformation" class="nav nav-list" style="text-align:center"></ul>
 		<?
 			if($user == $loggedInUser){
 		?>
@@ -326,17 +329,166 @@ $user = (isset($_GET['user'])) ? $_GET['user'] : $loggedInUser;
       </div><!--/.well -->
     </div><!--/span-->
 
-    <div id="friendsgroups" class="span8">
-      <div class="span5">
+
+    <div id="nofriends" style="display:none" class="span3 offset2"><? echo $user.' '.$noFriendsYet ?></div>
+    <div id="comparison" class="span5">
+        <div id="chart_div" style="width: 700px; height: 400px;">   
+          <div id="loadingIndicator_graph" style="background:url(./assets/img/ajax-loader.gif) no-repeat center center; height:100px; display:none">
+          </div>
+        </div>
+      </div>
+
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script> 
+
+  <? if($user != $loggedInUser){ ?>
+    <script type="text/javascript">
+      var friend = "<? echo $user?>";
+      var values = [];
+      var values2 = [];
+      var count=0;
+      var phen=[];
+
+      $.get('assets/includes/users.php?userStatistics=<? echo $_SESSION['name'] ?>', function(data) {
+        $('#loadingIndicator_graph').show();
+        if(data >= 400){
+            error_msg("<? echo $statisticsError ?>");
+            $('#loadingIndicator_graph').hide();
+        }else{
+          data = JSON.parse(data);
+          count=data.statistics.length;
+          for(i = 0; i < data.statistics.length; i++){
+            values[i]= Math.round(data.statistics[i].avg*100)/100;
+            phen[i]=data.statistics[i].phenomenon.name;
+          }
+        }
+      });
+
+      $.get('assets/includes/users.php?friendStatistics='+friend, function(data) {
+        if(data >= 400){
+            if(data == 401 || data == 403) noFriend();
+            else error_msg("<? echo $statisticsNotFound ?>");
+            $('#loadingIndicator_graph').hide();
+        }else{
+          data = JSON.parse(data);
+          for (h=0; h<count; h++ ){
+            values2[h]=0;
+          }
+          for(i = 0; i < data.statistics.length; i++){ 
+            for (j=0; j<count; j++ ){
+            if ((data.statistics[i].phenomenon.name)==phen[j]){ 
+              values2[j]= Math.round(data.statistics[i].avg*100)/100;
+              break;
+            }
+          }
+          }
+          if(data.statistics.length==0){
+            values2 = [0,0,0,0];
+          }
+          google.setOnLoadCallback(drawChart());
+        }
+      });
+
+
+      google.load("visualization", "1", {packages:["corechart"]});
+
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+          data.addColumn('string','Measurement');
+          data.addColumn('number', '<? echo $_SESSION['name'] ?>');
+          data.addColumn('number', friend);
+
+          data.addRows(count);
+               
+        for(i = 0; i < count; i++){
+          if(phen[i] == 'Rpm'){
+            phen[i] = phen[i]+' (100/min)';
+            values[i] = values[i]/100;
+            values2[i] = values2[i]/100;
+          }
+          data.setValue(i, 0, phen[i]);
+          data.setValue(i, 1, values[i]);
+          data.setValue(i, 2, values2[i]);
+        }
+     
+        var options = {
+          title: '<? echo $statistics ?>',
+          vAxis: {title: '',  titleTextStyle: {color: 'red'}}
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+        $('#loadingIndicator_graph').hide();
+      }
+    </script>  
+
+<? }else{ ?>
+   <script type="text/javascript">
+      var values = [];
+      var count=0;
+      var phen=[];
+
+      $.get('assets/includes/users.php?userStatistics=<? echo $_SESSION['name'] ?>', function(data) {
+        $('#loadingIndicator_graph').show();
+        if(data >= 400){
+            error_msg("<? echo $statisticsError ?>");
+            $('#loadingIndicator_graph').hide();
+        }else{
+          data = JSON.parse(data);
+          count=data.statistics.length;
+          for(i = 0; i < data.statistics.length; i++){
+            values[i]= Math.round(data.statistics[i].avg*100)/100;
+            phen[i]=data.statistics[i].phenomenon.name;
+          }
+          google.setOnLoadCallback(drawChart());
+        }
+      });
+
+      google.load("visualization", "1", {packages:["corechart"]});
+
+      function drawChart() {
+        var data = new google.visualization.DataTable();
+          data.addColumn('string','Measurement');
+          data.addColumn('number', '<? echo $_SESSION['name'] ?>');
+
+          data.addRows(count);
+               
+        for(i = 0; i < count; i++){
+          if(phen[i] == 'Rpm'){
+            phen[i] = phen[i]+' (100/min)';
+            values[i] = values[i]/100;
+          }
+          data.setValue(i, 0, phen[i]);
+          data.setValue(i, 1, values[i]);
+        }
+     
+        var options = {
+          title: '<? echo $statistics ?>',
+          vAxis: {title: '',  titleTextStyle: {color: 'red'}}
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+        $('#loadingIndicator_graph').hide();
+      }
+    </script>  
+
+
+<? } ?>
+    </div>
+</div>
+
+  <div class="container leftband" id="friendsgroups">
+      <div class="span4">
         <h2><? echo $friends ?></h2>
          <div id="loadingIndicator_friends" style="background:url(./assets/img/ajax-loader.gif) no-repeat center center; height:100px;"></div>
         <ul id="friends" style="margin-bottom: 10px; overflow-y:auto; max-height: 400px;">
         </ul>
       </div>
-      <div id="groups" class="span5">
+      <div class="span4">
         <h2><? echo $groups ?></h2>
+        <ul id="groups" style="margin-bottom: 10px; overflow-y:auto; max-height: 400px;">
+        </ul>
          <div id="loadingIndicator_groups" style="background:url(./assets/img/ajax-loader.gif) no-repeat center center; height:100px;"></div>
-      </div>
     </div>
   </div>
 </div>
