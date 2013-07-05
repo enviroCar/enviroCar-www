4c02ad76c1e620745cacb55113211fe84affc8c0
Dennis Wilhelm <dwilhelm89@googlemail.com>
2013-07-05 12:38:06 +0200

    Fixed Graph + ErrorHandling

--- graph.php
+++ graph.php
@@ -47,40 +47,56 @@ include('header.php');
 	var trackID = '<?php echo $_GET["id"]; ?>';
 	
 	function callPhens(){
-		$.get('http://giv-car.uni-muenster.de:8080/stable/rest/tracks/' + trackID + '/statistics', function(data) {
-			for(var i=0;i<data.statistics.length;i++){
-				phenName[i] = data.statistics[i].phenomenon.name;
-				}
-			callData();
-			}).fail(function (){
-				error_msg("<? echo $statisticsError ?>");
+		$.get('./assets/includes/users.php?atrackStatistics=' + trackID, function(data) {
+			if(data >= 400){
+		        console.log(data);
+		        if(data == 400){
+		            error_msg("<? echo $statisticsError ?>");
+		        }else if(data == 401 || data == 403){
+		          error_msg("<? echo $statisticsNotAllowed ?>")
+		        }else if(data == 404){
+		          error_msg("<? echo $statisticsNotFound ?>")
+		        }
+		        $('#loadingIndicator').hide();
+		    }else{
+				data = JSON.parse(data);
+				for(var i=0;i<data.statistics.length;i++){
+					phenName[i] = data.statistics[i].phenomenon.name;
+					}
+				callData();
 				$('#loadingIndicator').hide();
-			});
-		}
+			}
+
+		});
+	}
 	
 	function callData(){
-		$.get('http://giv-car.uni-muenster.de:8080/stable/rest/tracks/' + trackID, function(data) {
-		if(data >=400){
-			error_msg("Routes couldn't be loaded successfully.");
-			$('#loadingIndicator').hide();
-		}else{
-			tracks = data;
-			for(var i=0;i<phenName.length;i++){
-				$('#graphs').append('<div class="span5"><h2>'+phenName[i]+'</h2><div id="chart'+i+'" style="width: 500px; height: 400px;"></div><a class="btn" onclick="zoomIn('+i+')">Zoom in</a></div>');
-				phen[0] = ['time', eval('data.features[0].properties.phenomenons["'+phenName[i]+'"].unit')];
-				for(var j=0;j<data.features.length;j++){
-					serverTime[j] = data.features[j].properties.time;
-					time[j] = convertToLocalTime(serverTime[j]);
-					phen[j+1] = [time[j], eval('data.features['+j+'].properties.phenomenons["'+phenName[i]+'"].value')];
+		$.get('./assets/includes/users.php?atrack=' + trackID, function(data) {
+		    if(data >= 400){
+		      if(data == 400){
+		          error_msg("<? echo $routeError ?>");
+		      }else if(data == 401 || data == 403){
+		        error_msg("<? echo $routeNotAllowed ?>")
+		      }else if(data == 404){
+		        error_msg("<? echo $routeNotFound ?>")
+		      }
+		      $('#loadingIndicator').hide();
+		    }else{
+				data = JSON.parse(data);
+				tracks = data;
+				for(var i=0;i<phenName.length;i++){
+					$('#graphs').append('<div class="span5"><h2>'+phenName[i]+'</h2><div id="chart'+i+'" style="width: 500px; height: 400px;"></div><a class="btn" onclick="zoomIn('+i+')">Zoom in</a></div>');
+					phen[0] = ['time', eval('data.features[0].properties.phenomenons["'+phenName[i]+'"].unit')];
+					for(var j=0;j<data.features.length;j++){
+						serverTime[j] = data.features[j].properties.time;
+						time[j] = convertToLocalTime(serverTime[j]);
+						phen[j+1] = [time[j], eval('data.features['+j+'].properties.phenomenons["'+phenName[i]+'"].value')];
 					}
-				chartName = 'chart'+i;
-				drawChart(phen, chartName);
+					chartName = 'chart'+i;
+					drawChart(phen, chartName);
 				}
 			}
 			$('#loadingIndicator').hide();
-			}).fail(function (){
-				error_msg("<? echo $routeError ?>");
-				$('#loadingIndicator').hide();
 			});
 		}
 		
