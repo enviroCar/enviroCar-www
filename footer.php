80b4db2a5c5ee22c43bf7da3c45975ef55b61a2e
arne-broering <arneb@uni-muenster.de>
2013-08-12 14:27:52 +0200

    link to 'terms of use' was '#' - removed for now

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         <p class="pull-right"><a href="#"><? echo $footer_backtotop; ?></a></p>
         <span>2013 <?echo $envirocar; ?></span>
         <span> &middot; <a href="imprint.php"><? echo $footer_imprint; ?></a></span>
-        <span> &middot; <a href="#"><? echo $footer_terms; ?></a></span>
+        <!-- <span> &middot; <a href="#"><? echo $footer_terms; ?></a></span> -->
         <span> &middot; <a href="privacy.php"><? echo $footer_privacy; ?></a></span>
         <span> &middot; <a href="licensing.php"><? echo $footer_license; ?></a></span>
 		</div>
