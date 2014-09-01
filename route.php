5fa10a4320ad8917822ee75f9d20d8afa347444d
matthesrieke <m.rieke@52north.org>
2014-09-01 16:48:51 +0200

    add simple php proxy for access to OSM tiles

--- route.php
+++ route.php
@@ -763,8 +763,8 @@ function addSeries(series, axis){
 function initMap() {
 	
   var osm = new OpenLayers.Layer.OSM('<?php echo $route_baseLayer; ?>', [
-	"http://a.www.toolserver.org/tiles/bw-mapnik/${z}/${x}/${y}.png",
-	"http://b.www.toolserver.org/tiles/bw-mapnik/${z}/${x}/${y}.png"], {
+	"./assets/proxy/ba-simple-proxy.php?url=http%3A%2F%2Fa.www.toolserver.org%2Ftiles%2Fbw-mapnik%2F${z}%2F${x}%2F${y}.png",
+	"./assets/proxy/ba-simple-proxy.php?url=http%3A%2F%2Fb.www.toolserver.org%2Ftiles%2Fbw-mapnik%2F${z}%2F${x}%2F${y}.png"], {
 		crossOriginKeyword: null
 	});
   map.addLayer(osm);
