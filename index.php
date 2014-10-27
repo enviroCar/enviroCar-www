00173e10783b8133482f3292a8147b0a53cedf2b
ahitch <a.hitchcock@52north.org>
2014-10-27 13:35:53 +0100

    changed box text in index.php

--- index.php
+++ index.php
@@ -75,7 +75,8 @@ if(!is_logged_in()){
         <div class="span6">
           <h2><? echo $envirocar;?> <span class="muted"><? echo $index_here_we_go;?></span></h2>
           
-          <p><? echo $index_help_the_world;?></p>
+          <p>
+			<? echo $index_this_is_community;?></p>
         </div>
   <!--      <div class="span4">
           <h2><? echo $index_support_indiegogo;?></h2>
@@ -92,9 +93,7 @@ if(!is_logged_in()){
        <div class="span6" style="margin-left:10px;">
 <!--		<img class="featurette-image pull-right" src="./assets/img/enviroCarConceptOverview.png" style="width: 40%; padding: 3%" alt=""/>-->
 		<h2><? echo $index_be_a_citizen_scientist;?></h2>
-		<p>
-			<? echo $index_this_is_community;?>
-		</p>
+		<p><? echo $index_help_the_world;?></p>
 		</div>
       <div class="span5">
       <iframe width="480" height="340" style="z-index:-9999; border:none;" src="//www.youtube.com/embed/LTSuUEOfWa0?rel=0&amp;wmode=transparent" allowfullscreen></iframe>
