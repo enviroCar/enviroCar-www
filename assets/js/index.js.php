fdb08f32475f7a449e49438c39c51e0a2c36eff9
drifftr <drifftr@gmail.com>
2015-08-25 20:50:03 +0530

    fixed require.js issue

--- assets/js/index.js.php
+++ assets/js/index.js.php
@@ -17,6 +17,7 @@
 */
 ?>
 <script src="http://js.arcgis.com/3.14/"></script>
+<script src="./assets/leaflet.wms/lib/require.js" data-main="./assets/js/index.js.php"></script>
 <script type="text/javascript">
 
 $(document).ready(function() {
