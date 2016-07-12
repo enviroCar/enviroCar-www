98c305d72b3503d490ed9b4f0ddd00da3d4fe5a8
matthesrieke <m.rieke@52north.org>
2016-07-12 15:45:06 +0200

    minimize attribution

--- route.php
+++ route.php
@@ -607,7 +607,7 @@ function initMap() {
 		crossOriginKeyword: null
 	});
 
-	osm.attribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.';
+	osm.attribution = 'Tiles by <a href="http://stamen.com">Stamen</a> (<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>). Data by <a href="http://openstreetmap.org">OSM</a> (<a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>).';
 
   map.addLayer(osm);
   vectorLayer = new OpenLayers.Layer.Vector('<?php echo $route_drivenRoute; ?>');
